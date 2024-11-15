ba33480b0d34db082c5437f532c49c2ac44f6247
joshkoo1988 <joshkoo1988@gmail.com>
2024-11-15 18:24:12 -0500

    added block to block type and tests

--- src/markdown_blocks.py
+++ src/markdown_blocks.py
@@ -1,3 +1,10 @@
+block_type_paragraph = "paragraph"
+block_type_heading = "heading"
+block_type_code = "code"
+block_type_quote = "quote"
+block_type_olist = "ordered_list"
+block_type_ulist = "unordered_list"
+
 def markdown_to_blocks(markdown):
     split_markdown = markdown.split("\n\n")
     stripped_markdown = []
@@ -7,3 +14,55 @@ def markdown_to_blocks(markdown):
             stripped_markdown.append(stripped_line)
     return stripped_markdown
     
+
+def block_to_block_type(block):
+    # heading block
+    if block[0] == "#":
+        hash_count = 0
+        for char in block:
+            if char == "#":
+                hash_count += 1
+            else:
+                break
+        if hash_count <= 6 and block[hash_count] == " ":
+            return block_type_heading
+        
+    # code block 
+    if block.startswith("```"):
+        split_block = block.split("\n")
+        if len(split_block) > 1 and split_block[0].startswith("```")and split_block[-1].startswith("```"):
+            return block_type_code
+    
+    # quote block
+    if block[0] == ">":
+        split_block = block.split("\n")
+        for line in split_block:
+            if not line.startswith(">"):
+                break
+        else:
+            return block_type_quote
+    
+    #unordered list
+    if block[0] == "*" or block[0] == "-":
+        split_block = block.split("\n")
+        for line in split_block:
+            if not (line.startswith("* ") or line.startswith("- ")):
+                break
+        else:
+            return block_type_ulist
+    
+    #ordered list
+    if block.startswith("1."):
+        split_block = block.split("\n")
+        counter = 1
+        for line in split_block:
+            if line.startswith(f"{counter}. "):
+                counter += 1
+            else:
+                break
+        else:
+            return block_type_olist
+    
+    #if none of the above return paragraph
+    else:
+        return block_type_paragraph
